d85199d0b6d98233972ff24757f96787447b3922
MateoGuilleng <mmateoglln@gmail.com>
2024-12-22 21:59:43 -0500

    Fix bug in message API route

--- src/app/api/messages/route.js
+++ src/app/api/messages/route.js
@@ -11,6 +11,6 @@ export async function GET() {
     
     return NextResponse.json(messages); // Devuelve los mensajes como respuesta JSON
   } catch (error) {
-    return NextResponse.error(); // Si hay un error, devuelve un error
+    return NextResponse.error(); // Si hay un error, devuelve un errorr
   }
 }
